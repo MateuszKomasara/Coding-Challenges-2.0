43c411e0c91d9a842ddfbcca01a3938cbb5330c8
Mateusz Komasara <mkomasara@hotmail.com>
2022-03-02 13:57:41 -0500

    Coding challenge and solution 2022-03-02

---
+++ CC 2022-03-01/app.js
@@ -0,0 +1,10 @@
+// Challenge:
+
+// Task
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+
+
+// Solution:
+
+const adjacentElementsProduct = array => Math.max(...array.map((x,i)=>x*array[i+1]).slice(0,-1));
