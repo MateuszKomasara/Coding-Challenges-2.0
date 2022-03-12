89f40f972c370aa5668f8ad7585b8f85f4bd538d
Mateusz Komasara <mkomasara@hotmail.com>
2022-03-12 12:25:42 -0500

    Coding challenge and solution 2022-03-12

---
+++ CC 2022-03-12/app.js
@@ -0,0 +1,16 @@
+// 7kyu Flatten and Sort
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+// Example:
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+function flattenAndSort(array) {
+  let merged = [].concat.apply([], array);
+    return merged.sort((a,b) => a-b);
+;  
+  
+}
+
+function flattenAndSort(array) {
+  return [].concat(...array).sort((a,b) => a - b);
+}
