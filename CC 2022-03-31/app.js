85745fb62886909d635da532d485bc3a3c3ba8a0
Mateusz Komasara <mkomasara@hotmail.com>
2022-03-31 17:03:36 -0400

    Coding challenge and solution 2022-03-31

---
+++ CC 2022-03-31/app.js
@@ -0,0 +1,31 @@
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+// My Answer.
+
+function basicOp(operation, value1, value2)
+{
+  if( operation === '+'){
+ return value1 + value2;
+  } else if( operation === '-'){
+ return value1 - value2;
+  } else if( operation === '*'){
+  return value1 * value2;
+  } else if( operation === '/'){
+  return value1 / value2;
+ }
+  }
+
+// More concise refactored answer.
+
+function basicOp(operation, value1, value2)
+{
+  return eval(value1 + operation + value2);
+}
