573064fc19ffa7b0a97f1eb50cd1e07f0b999f13
Mateusz Komasara <mkomasara@hotmail.com>
2022-04-20 14:24:56 -0400

    Coding challenge and solution for 2022-04-20

--- CC 2022-04-20/app.js
+++ CC 2022-04-20/app.js
@@ -3,3 +3,17 @@
 // For example:
 // gimme([2, 3, 1]) => 0
 
+let gimme = function (inputArray) {
+    if ((inputArray[0] < inputArray[1] && inputArray[0] > inputArray[2])
+      || (inputArray[0] > inputArray[1] && inputArray[0] < inputArray[2]))
+      return 0;
+      
+    if ((inputArray[1] < inputArray[0] && inputArray[1] > inputArray[2])
+      || (inputArray[1] > inputArray[0] && inputArray[1] < inputArray[2]))
+      return 1;
+      
+    if ((inputArray[2] < inputArray[0] && inputArray[2] > inputArray[1])
+      || (inputArray[2] > inputArray[0] && inputArray[2] < inputArray[1]))
+      return 2;
+  };
+  
