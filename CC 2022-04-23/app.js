24e6ca8a9518b90e59048793f6e5259a5bbdce66
Mateusz Komasara <mkomasara@hotmail.com>
2022-04-23 14:48:55 -0400

    updated coding challenge and solution for 2022-04-23

---
+++ CC 2022-04-23/app.js
@@ -0,0 +1,10 @@
+// Highest and Lowest 7kyu
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+
+function highAndLow(numbers){
+let nums = numbers.split(" ");
+    return `${Math.max(...nums)} ${Math.min(...nums)}`;
+}
+
+
