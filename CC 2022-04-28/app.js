f8fa524be3ab6e7f0e712c24f511a3248de4b7e9
Mateusz Komasara <mkomasara@hotmail.com>
2022-04-28 15:37:00 -0400

    COding challenge and refactored solution 2022-04-28

--- CC 2022-04-28/app.js
+++ CC 2022-04-28/app.js
@@ -8,3 +8,11 @@
 // titanic.isWorthIt() // return false
 // reation.
 // Good luck!
+
+function Ship(draft,crew) {
+    this.draft = draft,
+    this.crew = crew,
+    this.isWorthIt = function() {
+      return this.draft - this.crew * 1.5 > 20 ;
+    }
+ }
