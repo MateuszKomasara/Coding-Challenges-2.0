a58a4e3334ed2f523c53ac846d76c6f9504af4ec
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-06 16:59:22 -0400

    Coding challenge with refactored solution for 2022-05-06

--- CC 2022-05-06/app.js
+++ CC 2022-05-06/app.js
@@ -7,8 +7,15 @@
 
 
 function sumTwoSmallestNumbers(numbers) {  
-  let first = Math.min(...numbers)
-  numbers.splice(numbers.indexOf(first), 1)
-  let second = Math.min(...numbers)
-  return first + second
+    let first = Math.min(...numbers)
+    numbers.splice(numbers.indexOf(first), 1)
+    let second = Math.min(...numbers)
+        return first + second
+}
+
+const sumTwoSmallestNumbers = numbers => {
+    let first = Math.min(...numbers)
+    numbers.splice(numbers.indexOf(first), 1)
+    let second = Math.min(...numbers)
+        return first + second
 }
