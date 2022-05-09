84fd001ab8b98b5f88004a9ceb9edc2227986259
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-09 17:18:04 -0400

    Coding challenge and refactored solution 2022-05-09

--- CC 2022-05-09/app.js
+++ CC 2022-05-09/app.js
@@ -7,3 +7,9 @@ function squareArea(A) {
   let area = Math.pow(a, 2);
   return Number(area.toFixed(2));
 }
+
+const squareArea = A => {
+    let a = A / (2 * Math.PI * (90 / 360));
+  let area = Math.pow(a, 2);
+  return Number(area.toFixed(2));
+}
