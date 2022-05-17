caeb4d2c1a9b87d7997ed99a1608255cb19114a7
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-16 21:03:22 -0400

    Coing challenge and refactored solution 2022-05-16

--- CC 2022-05-16/app.js
+++ CC 2022-05-16/app.js
@@ -16,3 +16,9 @@ function getGrade (s1, s2, s3) {
     else if (avg < 90) return "B";
     else return "A";
 }
+
+function getGrade (s1, s2, s3) {
+    var s = (s1 + s2 + s3) / 3
+    return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+  }
+  
