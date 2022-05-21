3612f87efb00e23768d8f523530405ef20dac3a5
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-21 19:27:43 -0400

    Coding challenge and refactored solution 2022-05-21

--- CC 2022-05-21/app.js
+++ CC 2022-05-21/app.js
@@ -11,4 +11,18 @@ function strCount(str, letter){
   return str.split(letter).length-1
 }
 
-
+function strCount(str, letter){  
+  
+    let count = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == letter)
+        count++;
+    }
+    
+    return count;
+    
+  }
+  
+  const strCount = (str, letter) => str.split(letter).length - 1;
+  
