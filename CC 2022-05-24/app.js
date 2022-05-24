2cfa8242ecf4031cae1ad3febb695bea39be2241
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-24 17:35:02 -0400

    coding challenge and refactored solution for 2022-05-24

--- CC 2022-05-24/app.js
+++ CC 2022-05-24/app.js
@@ -1,4 +1,6 @@
 // Create a function to check if a string is a paliindrome, make sure it's case insensitive.
 
+//refactored solution
+
 const isPalindrome = x => x.toLowerCase() === x.toLowerCase().split('').reverse().join('');
 
