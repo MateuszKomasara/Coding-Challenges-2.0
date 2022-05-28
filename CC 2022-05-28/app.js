c2bddef37c77e27c65bda09c431f3100c21e4f94
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-28 14:42:17 -0400

    Coding challenge and refactored solution 2022-05-28

--- CC 2022-05-28/app.js
+++ CC 2022-05-28/app.js
@@ -1,8 +1,8 @@
 // Given a non-negative integer,Â 3Â for example, return a string with a murmur:Â "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
 let countSheep = function (num){
-  let str = "";
-  for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
- return str;
+    let str = "";
+    for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+        return str;
 }
 
