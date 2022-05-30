e2816e1dcae0e8404467435d4670eadf3a277ed9
Mateusz Komasara <mkomasara@hotmail.com>
2022-05-30 15:01:41 -0400

    Coding challenge and refactored solution 2022-05-30

--- CC 2022-05-30/app.js
+++ CC 2022-05-30/app.js
@@ -8,4 +8,11 @@ function fakeBin(x){
   return result;
 }
 
+const fakeBin = x => {
+    let result = '';
+  for(let i = 0; i < x.length; i++){
+    x[i] < 5 ? result += 0 : result += 1;
+  }
+  return result;
+}
 
