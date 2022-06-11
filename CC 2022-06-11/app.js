f5b598ed62705434cf53fc0e0ea4f5e2149e7af7
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-11 18:44:59 -0400

    Coding challenge and solution 2022-06-11

---
+++ CC 2022-06-11/app.js
@@ -0,0 +1,29 @@
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+
+function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
+  }
+  
+//   More concise refactored!!
+  sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
+  
+  
+  
