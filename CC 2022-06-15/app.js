1a391d555d39bbf18ffe08635fc7dbf6ebd261c5
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-15 18:59:06 -0400

    Coding challenge and refactored solution 2022-06-15

--- CC 2022-06-15/app.js
+++ CC 2022-06-15/app.js
@@ -5,7 +5,7 @@
 // true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
 // Task
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
-
+// refactored solution
 const xor = (a,b) => a === b ? false : a === true && b === false ? true : a === false && b === true;
 
 
