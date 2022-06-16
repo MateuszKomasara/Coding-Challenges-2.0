b70b202b644749cd3e1984bdf95b8b36adcdec5a
Mateusz Komasara <mateuszkomasara@Mateuszs-MacBook-Air.local>
2022-06-16 17:23:20 -0400

    Co ding Challenge and solution on 06-16

---
+++ CC 2022-06-16/app.js
@@ -0,0 +1,6 @@
+// 8kyu Enumerable Magic #25 - Take the First N Elements
+
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+
+const take = (arr,n) => arr.slice(0,n);
