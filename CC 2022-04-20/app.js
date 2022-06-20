da2fc1f9bcf408254f9a00c28d19013aeae236fc
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-20 14:22:28 -0400

    Coding challenge 2022-04-20

---
+++ CC 2022-04-20/app.js
@@ -0,0 +1,5 @@
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+// For example:
+// gimme([2, 3, 1]) => 0
+
