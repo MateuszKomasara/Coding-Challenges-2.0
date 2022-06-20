34750a5aa6b67e7e9d13b1899da2965650001810
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-20 14:33:17 -0400

    Coding challenge for 2022-04-22

---
+++ CC 2022-04-22/app.js
@@ -0,0 +1,6 @@
+// 7kyu Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+// Hi < firstName here >, what do you like the most about < language here >
+
