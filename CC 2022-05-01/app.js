5777f40aa2922f65797e49569177a8b7c2a17cff
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-21 16:08:28 -0400

    COding challenge and refactored solution for 2022-05-01

--- CC 2022-05-01/app.js
+++ CC 2022-05-01/app.js
@@ -4,3 +4,10 @@
 // Note: You can expect all of the inputs to be the same length.
 
 
+function tripleTrouble(one, two, three) {
+    let result = "";
+    for (let i = 0; i < one.length; i++) {
+        result += one.charAt(i) + two.charAt(i) + three.charAt(i);
+    }
+    return result;
+    }
