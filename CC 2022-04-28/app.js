4d950a65f3a2abf3892d476290a5ffb36a78e409
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-21 15:35:31 -0400

    COding challenge 2022-04-28

---
+++ CC 2022-04-28/app.js
@@ -0,0 +1,10 @@
+// 8kyu Object Oriented Piracy
+// This Kata teaches you the very basic of method 
+
+// Taking into account that an average crew member on board adds 1.5 units to the draft, a ship that has a draft of more than 20 without crew is considered worthy to loot. Any ship weighing that much must have a lot of booty!
+// Add the method
+// isWorthIt
+// to decide if the ship is worthy to loot. For example:
+// titanic.isWorthIt() // return false
+// reation.
+// Good luck!
