601e62e256d9c4430e7965b68a49cf0851bf9124
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-23 20:56:38 -0400

    Coding challenge and solution for 2022-05-15

---
+++ CC 2022-05-15/app.js
@@ -0,0 +1,7 @@
+// Clock showsÂ hÂ hours,Â mÂ minutes andÂ sÂ seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+function past(h, m, s){
+    return ((h*3600)+(m*60)+s)*1000;
+}
+
