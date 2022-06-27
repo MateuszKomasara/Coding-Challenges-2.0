48f3c6a6a317d52e21e2a42e999021189d21393c
Mateusz Komasara <mkomasara@hotmail.com>
2022-06-27 13:50:48 -0400

    Coding challenge and solution 2022-06-01

---
+++ CC 2022-06-01/app.js
@@ -0,0 +1,7 @@
+// Given that person A loves to cycle and needs to stay hydrated by consuming 0.5 litres of water per hour. How would you calculate the amount of water consumed if given the amount of hours.
+
+// Also round down to the nearest value.
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
