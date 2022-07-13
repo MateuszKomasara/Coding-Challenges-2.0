db84e6c2e631034da803c7eb086915a84f458e8d
Mateusz Komasara <mkomasara@hotmail.com>
2022-07-13 13:59:31 -0400

    coding challenge and solution 2022-07-13

---
+++ CC 2022-07-13/app.js
@@ -0,0 +1,11 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(str) {
+	return str.split('').map((cur,ind) => (cur.toUpperCase() + cur.toLowerCase().repeat(ind))).join('-');
+}
+
