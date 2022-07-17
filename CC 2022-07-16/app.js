5b56d27f197582b6b64a07a342462eb75389bb11
Mateusz Komasara <mkomasara@hotmail.com>
2022-07-17 17:00:17 -0400

    Coding challenge and solution 2022-07-17

---
+++ CC 2022-07-16/app.js
@@ -0,0 +1,15 @@
+// Your task is to write function findSum.
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+// For example:
+// findSum(5) should return 8 (3 + 5)
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+
+function findSum(n) {
+  let result = 0;
+  for (let i = 0; i <= n; i += 1) {
+    if (i % 3 ===0 || i % 5 === 0) result += i
+  }
+  return result
+}
+
---
+++ CC 2022-07-17/app.js
@@ -0,0 +1,10 @@
+// 7 kyu Rreturn the closest number divisible by 10
+// Given a number return the closest number to it that is divisible by 10.
+
+// Example input:
+
+// 22
+// 25
+// 37
+
+const closestMultiple10 = num => Math.round(num /10)* 10;
