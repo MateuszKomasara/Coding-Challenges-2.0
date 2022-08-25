97780a2b84d29e658380c8471d0c1bf9b7bfd9cd
Mateusz Komasara <mkomasara@hotmail.com>
2022-08-25 14:09:19 -0400

    Coding challenge and solution 2022-08-25

---
+++ CC 2022-08-25/app.js
@@ -0,0 +1,12 @@
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+function splitAndMerge(string, s) {
+    return string.split(' ').map(index => index.split('').join(s)).join(' ');
+  }
