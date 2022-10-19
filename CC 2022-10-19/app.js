3056b4775a1594e68d98a74143f747467b70660a
Mateusz Komasara <mkomasara@hotmail.com>
2022-10-19 15:07:31 -0400

    Coding challenge and solution 2022-10-19

---
+++ CC 2022-10-19/app.js
@@ -0,0 +1,8 @@
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+// Example:
+
+// keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b',
+
+
+const keysAndValues = data => [Object.keys(data), Object.values(data)]
