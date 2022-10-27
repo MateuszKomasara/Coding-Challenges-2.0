e6cad0168986a6065bf8b6148faf5652adfd708e
Mateusz Komasara <mkomasara@hotmail.com>
2022-10-27 14:17:11 -0400

    Coding challenge and solution 2022-10-27

---
+++ CC 2022-10-27/app.js
@@ -0,0 +1,22 @@
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+// My original solution :
+
+const greet = name => {
+    let start = name.slice(0,1).toUpperCase();
+    let rest = name.slice(1).toLowerCase()
+    return `Hello ${start}${rest}!`
+  }
+
+//   alternative refactored solution :
+
+  let greet = function(name) {
+    return `Hello ${name.slice(0,1).toUpperCase()}${name.slice(1).toLowerCase()}!`;
+  };
+  
+  
