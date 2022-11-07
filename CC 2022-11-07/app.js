fe0d5dc4f379837b56892c3f4b85a7e9d27e3ae6
Mateusz Komasara <mkomasara@hotmail.com>
2022-11-07 08:53:03 -0500

    Coding challenge and solution 2022-11-07

---
+++ CC 2022-11-07/app.js
@@ -0,0 +1,15 @@
+// When provided with a String, capitalize all vowels
+
+// For example:
+
+// Input : "Hello World!"
+
+// Output : "HEllO WOrld!"
+
+// Note: Y is not a vowel in this kata.
+
+
+function swap (string) {
+    let vows = 'aeiou';
+     return string.split('').map(x=> vows.includes(x)? x.toUpperCase(): x).join('');
+   }
