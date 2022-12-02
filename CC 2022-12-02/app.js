80b8e0db1f6f1053b5f6698a59070424f7111bf3
Mateusz Komasara <mkomasara@hotmail.com>
2022-12-02 10:28:14 -0500

    Coding challenge and solution 2022-12-02

---
+++ CC 2022-12-02/app.js
@@ -0,0 +1,10 @@
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
+
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 14
+
+function getSumOfDigits(integer) {
+    return integer.toString().split('').map(Number).reduce((a,b)=> a + b)
+  }
