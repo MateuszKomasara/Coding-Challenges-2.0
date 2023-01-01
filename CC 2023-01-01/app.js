bf08efee5ba03f12cb74456c21c04222060c318e
Mateusz Komasara <mkomasara@hotmail.com>
2023-01-01 12:08:00 -0500

    Coding challenge and solution 2023-01-01

---
+++ CC 2023-01-01/app.js
@@ -0,0 +1,22 @@
+// Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+
+
+
+function getMax1()
+{
+  let max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  let max2 = 
+  {
+    name: 'Max Headroom'
+  }
+  return max2;
+}
