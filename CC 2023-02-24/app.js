402e9c94990328c091209d632b7b82e8a1a274ad
Mateusz Komasara <mkomasara@hotmail.com>
2023-02-27 11:11:08 -0500

    Coding challenge and solution 2023-02-27

--- CC 2023-02-24/app.js
+++ CC 2023-02-24/app.js
@@ -21,3 +21,5 @@ function numberOfPairs(gloves){
     
     return numPairs;
   }
+
+
--- CC 2023-02-26/app.js
+++ CC 2023-02-26/app.js
@@ -13,3 +13,8 @@
 function spinWords(string){
   return string.split(' ').map(x => x.length >=5? x.split('').reverse().join(''): x).join(' ');
   }
+
+
+  
+
+    
