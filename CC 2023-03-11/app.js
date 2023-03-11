6fecca4bdc1b5a93fc1d091faa9edaa5b6bddd29
Mateusz Komasara <mkomasara@hotmail.com>
2023-03-11 13:26:26 -0500

    Coding challenge and solution 2023-03-11

---
+++ CC 2023-03-11/app.js
@@ -0,0 +1,18 @@
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+
+
+function maxProduct(numbers, size){
+    return numbers.sort((a,b)=> b-a).slice(0,size).reduce((prev,cur)=> prev * cur)
+   }
