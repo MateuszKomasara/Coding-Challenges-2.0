3f83ebf233693fec4e20dcb737f5d5fe1c4e2a4c
Mateusz Komasara <mkomasara@hotmail.com>
2023-04-27 19:21:16 -0400

    Coding challenge and solution 2023-04-27

---
+++ CC 2023-04-27/app.js
@@ -0,0 +1,16 @@
+ 
+// Challenge
+
+// Write a function that doubles every second integer in a list, starting from the left.
+// Example:
+// For input array/list :
+// [1,2,3,4]
+// the function should return :
+// [1,4,3,8]
+
+
+// Solution:
+
+function doubleEveryOther(a) {
+  return a.map((x,i,arr)=> i % 2===1? x * 2: x)
+}
