4cfae76d1b7b414dbe635a85ad0e339a9e56cbe8
Mateusz Komasara <mkomasara@hotmail.com>
2023-06-07 09:35:47 -0400

    Added comments and explanations to challenge completed on 06-06

--- CC 2023-06-06/app.js
+++ CC 2023-06-06/app.js
@@ -8,3 +8,13 @@
 // const REGEXP = /./;
 
 const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
+
+
+// Explanation of the regular expression:
+// ^ asserts the start of the string.
+// (?=.*[a-z]) is a positive lookahead that ensures the presence of at least one lowercase letter.
+// (?=.*[A-Z]) is a positive lookahead that ensures the presence of at least one uppercase letter.
+// (?=.*\d) is a positive lookahead that ensures the presence of at least one digit.
+// [a-zA-Z\d]{6,} matches any alphanumeric character (both lowercase and uppercase letters, as well as digits) and ensures a minimum length of 6 characters.
+// $ asserts the end of the string.
+// So, when using this regular expression to validate a password, it will only pass if it meets all the given criteria: at least six characters long, contains a lowercase letter, contains an uppercase letter, contains a digit, and only contains alphanumeric characters
