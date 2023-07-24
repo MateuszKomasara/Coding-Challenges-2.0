864f4ad55a3680a4c84c8474adbcb69004bf54c8
Mateusz Komasara <mkomasara@hotmail.com>
2023-07-24 19:21:11 -0400

    Coding challenge and solution 2023-07-24

---
+++ CC 2023-07-24/app.js
@@ -0,0 +1,19 @@
+// Challenge:
+
+// Task
+// You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+// In practice, you should implement this function:
+// function eliminateUnsetBits(number);
+// Examples
+// eliminateUnsetBits("11010101010101") ->  255 (= 11111111)
+// eliminateUnsetBits("111") ->  7
+// eliminateUnsetBits("1000000") -> 1
+// eliminateUnsetBits("000") -> 0
+
+
+// Solution:
+
+function eliminateUnsetBits(number) {
+  let bins = number.replace(/0/g, '');
+  return bins? parseInt(bins,2) : 0
+}
