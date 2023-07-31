e5660d65416b8e27bdef0ca735e743dba08cd428
Mateusz Komasara <mkomasara@hotmail.com>
2023-07-31 06:43:52 -0400

    Coding challenge and solution 2023-07-31

---
+++ CC 2023-07-31/app.js
@@ -0,0 +1,27 @@
+// Challenge :
+
+// Task:
+// Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+// Examples:
+// twoArePositive(2, 4, -3) == true
+// twoArePositive(-4, 6, 8) == true
+// twoArePositive(4, -6, 9) == true
+// twoArePositive(-4, 6, 0) == false
+// twoArePositive(4, 6, 10) == false
+// twoArePositive(-14, -3, -4) == false
+
+
+
+// Solution:
+
+function twoArePositive(a, b, c) {
+  let arr = [a,b,c];
+  let pos =  arr.filter(x => x > 0).length;
+    return pos === 2 ? true : false;
+}
+
+// More concise solution:
+
+function twoArePositive(a, b, c) {
+  return [...arguments].filter(i => i > 0).length === 2;
+};
