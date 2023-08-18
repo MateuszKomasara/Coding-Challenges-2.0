b19050e8d0367544465a52daa06e68bab28efa44
Mateusz Komasara <mkomasara@hotmail.com>
2023-08-18 12:02:09 -0400

    Coding challenge and solution 2023-08-18

---
+++ CC 2023-08-18/app.js
@@ -0,0 +1,15 @@
+// You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+// Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+
+// Examples
+// ["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+// ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+
+function tailSwap(arr) {
+    let  head = arr.map(x => x.split(':')).map(c => c.slice(0,1))
+    let  tails = arr.map(x => x.split(':')).map(c => c.slice(1))
+    return [`${head[0]}:${tails[1]}`, `${head[1]}:${tails[0]}`]
+   
+  }
+  
