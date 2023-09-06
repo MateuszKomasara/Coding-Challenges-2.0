a52810c08967b2f227d8e9f46c532efdcff717f4
Mateusz Komasara <mkomasara@hotmail.com>
2023-09-06 09:23:55 -0400

    Coding challenge and solution 2023-09-06

---
+++ CC 2023-09-06/app.js
@@ -0,0 +1,12 @@
+// This Kata is intended as a small challenge for my students
+
+// Create a function that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+// Example (Input --> Output)
+
+// "drake" --> "drk"
+// "aeiou" --> ""
+// remove_vowels("drake") // => "drk"
+// remove_vowels("aeiou") // => ""
+
+const removeVowels = str => str.replace(/[aeiou]/gi, '')
