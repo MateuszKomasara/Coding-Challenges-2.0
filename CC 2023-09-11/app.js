3f80c61d8b39e94d4625833959908b4ca596a0b8
Mateusz Komasara <mkomasara@hotmail.com>
2023-09-11 15:27:32 -0400

    Coding challenge and solution 2023-09-11

---
+++ CC 2023-09-11/app.js
@@ -0,0 +1,12 @@
+// Task
+// Coding in function countAnimals. function accept two parameters: animals, a string contains some animals; count, an array list of which animals we should count. The result should be a number array.
+
+// Examples
+// countAnimals("dog,cat",["dog","cat"]); //=> [1,1]
+// countAnimals("dog,cat",["dog","cat","pig"]); //=> [1,1,0]
+// countAnimals("dog,dog,cat",["dog","cat"]); //=> [2,1]
+// countAnimals("dog,dog,cat",["pig","cow"]); //=> [0,0]
+
+function countAnimals(animals,count){
+    return count.map(animal => (animals.match(new RegExp(animal, "g")) || []).length);
+  }
