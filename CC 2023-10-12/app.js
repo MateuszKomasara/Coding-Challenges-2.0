31a302251f4899ace580362e7f7ce75c8732a89c
Mateusz Komasara <mkomasara@hotmail.com>
2023-10-12 15:07:06 -0400

    Coding challenge and solution 2023-10-12

---
+++ CC 2023-10-12/app.js
@@ -0,0 +1,21 @@
+// Challenge:
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+
+// Solution:
+
+function digitalRoot(n) {
+   n = n.toString();
+  while(n.length > 1){
+   n =  n.split('').map(Number).reduce((a,b)=> a+b,0).toString()
+  }
+  return Number(n)
+
+}
+
