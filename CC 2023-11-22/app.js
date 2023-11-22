9af182b55d4877cb020d5ec37fbda2676839939e
Mateusz Komasara <mkomasara@hotmail.com>
2023-11-22 14:46:57 -0500

    Coding challenge and solution 2023-11-22

---
+++ CC 2023-11-22/app.js
@@ -0,0 +1,32 @@
+// Some people are standing in a row in a park. There are trees between them which cannot be moved.
+
+// Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+
+// Example
+// For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+
+// [-1, 150, 160, 170, -1, -1, 180, 190].
+
+// Input/Output
+// [input] integer array a
+
+// If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
+
+// Constraints:
+
+// 5 ≤ a.length ≤ 30,
+
+// -1 ≤ a[i] ≤ 200.
+
+// [output] an integer array
+
+// Sorted array a with all the trees untouched.
+
+function sortByHeight(arr) {
+    const nonNeg = arr.filter(x => x !== -1);
+     const sortedNonNeg = nonNeg.sort((a,b)=> a-b);
+     
+     let curInd = 0;
+     return arr.map(value => value === -1? value : sortedNonNeg[curInd++])
+     
+   }
